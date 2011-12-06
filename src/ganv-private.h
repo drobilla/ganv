9646e7a855c1c0741ba4fec6c3dd2dc48e174e17
David Robillard <d@drobilla.net>
2011-12-06 21:17:18 +0000

    Trim license header.
    git-svn-id: http://svn.drobilla.net/lad/trunk/ganv@3822 a436a847-0d15-0410-975c-d299462d15a1

--- src/ganv-private.h
+++ src/ganv-private.h
@@ -1,18 +1,16 @@
 /* This file is part of Ganv.
  * Copyright 2007-2011 David Robillard <http://drobilla.net>
  *
- * Ganv is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Ganv is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
  *
- * Ganv is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * details.
+ * Ganv is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Ganv.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with Ganv.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef GANV_PRIVATE_H
