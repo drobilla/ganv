27d3c2b522c712e7ee6575cba2130c53338361fd
David Robillard <d@drobilla.net>
2014-08-30 05:48:42 +0000

    More obvious highlighting.
    git-svn-id: http://svn.drobilla.net/lad/trunk/ganv@5460 a436a847-0d15-0410-975c-d299462d15a1

--- src/node.c
+++ src/node.c
@@ -288,11 +288,12 @@ ganv_node_get_draw_properties(const GanvNode* node,
 
 	if (impl->selected) {
 		*dash_length  = 4.0;
-		*border_color = highlight_color(impl->border_color, 0x20);
+		*border_color = highlight_color(impl->border_color, 0x40);
 	}
 
 	if (impl->highlighted) {
-		*border_color = highlight_color(impl->border_color, 0x20);
+		*border_color = highlight_color(impl->border_color, 0x40);
+		*fill_color   = highlight_color(impl->fill_color, 0x10);
 	}
 }
 
